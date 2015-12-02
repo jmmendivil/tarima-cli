a6696e87195e1d9e685a98a7982ed986880e07c7
Alvaro Cabrera <pateketrueke@gmail.com>
2015-12-02 11:40:22 -0600

    Basic one-to-one compliation with rename support; fix

--- bin/compile-files.js
+++ bin/compile-files.js
@@ -1,5 +1,33 @@
+var fs = require('fs-extra'),
+    path = require('path'),
+    tarima = require('tarima'),
+    AsyncParts = require('async-parts');
+
 module.exports = function compileFiles(options, files, cb) {
-  console.log('TODO: iterate and compile everything');
-  console.log(options, files);
-  cb();
+  var chain = new AsyncParts();
+
+  function append(file) {
+    return function(next) {
+      var filepath = path.join(options.src, file),
+          partial = tarima.load(filepath);
+
+      var data = partial.params.options.data || {},
+          view = partial.data(data);
+
+      var ext = options.rename[partial.params.ext] || partial.params.ext,
+          dest = path.join(options.dest, file.replace(/\..+?$/, '.' + ext));
+
+      fs.outputFileSync(dest, view.source);
+
+      next();
+    };
+  }
+
+  files.forEach(function(src) {
+    chain.then(append(src));
+  });
+
+  chain.run(function(err) {
+    cb(err);
+  });
 };
